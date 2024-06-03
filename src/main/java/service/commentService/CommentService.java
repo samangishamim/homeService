0021266de81c1eb315c479d846a25a30503445c3
shamim <shamimsamangi5@gmail.com>
2024-06-03 16:39:15 +0330

    add al the methods on the interface class and implement class for comment

--- src/main/java/service/commentService/CommentService.java
+++ src/main/java/service/commentService/CommentService.java
@@ -3,5 +3,12 @@ package service.commentService;
 import base.service.BaseService;
 import model.Comment;
 
+import java.util.List;
+
 public interface CommentService extends BaseService<Comment,Long> {
+    void addComment(Long proposalId, Long customerId, String comment, int rating);
+
+    List<Comment> getCommentsForProposal(Long proposalId);
+
+    List<Comment> getCommentsForCustomer(Long customerId);
 }
--- src/main/java/service/commentService/CommentServiceImpl.java
+++ src/main/java/service/commentService/CommentServiceImpl.java
@@ -2,11 +2,55 @@ package service.commentService;
 
 import base.service.BaseServiceImpl;
 import model.Comment;
+import model.Customer;
+import model.Order;
+import model.Proposal;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import repository.commentRepository.CommentRepository;
+
+import java.util.List;
 
 public class CommentServiceImpl extends BaseServiceImpl<Comment, Long, CommentRepository> implements CommentService {
     public CommentServiceImpl(CommentRepository repository, SessionFactory sessionFactory) {
         super(repository, sessionFactory);
+    }
+
+
+    @Override
+    public void addComment(Long proposalId, Long customerId, String comment, int rating) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            Proposal proposal = session.get(Proposal.class, proposalId);
+            Customer customer = session.get(Customer.class, customerId);
+            Comment commentEntity = Comment.builder()
+                    .comment(comment)
+                    .rating(rating)
+                    .proposal(proposal)
+                    .customer(customer)
+                    .build();
+            repository.saveOrUpdate(commentEntity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public List<Comment> getCommentsForProposal(Long proposalId) {
+        return repository.findCommentsByProposalId(proposalId);
+    }
+    @Override
+    public List<Comment> getCommentsForCustomer(Long customerId) {
+        return repository.findCommentsByCustomerId(customerId);
     }
 }
