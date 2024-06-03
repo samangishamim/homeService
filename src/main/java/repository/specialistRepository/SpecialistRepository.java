3af0cdd29ed3c40601916e6dedf58e57341cbb5b
shamim <shamimsamangi5@gmail.com>
2024-06-03 18:17:07 +0330

    add the others methods for specialist repository

--- src/main/java/repository/specialistRepository/SpecialistRepository.java
+++ src/main/java/repository/specialistRepository/SpecialistRepository.java
@@ -13,5 +13,10 @@ public interface SpecialistRepository  extends BaseRepository<Specialist, Long> 
     Optional<List<Specialist>> findByProposalId(Long proposalId);
 
     Optional<Specialist> findByOrderIdAndProposalId(Long orderId, Long proposalId);
+    List<Specialist> getAllSpecialists();
+    Specialist addSpecialist(Specialist specialist);
+    void deleteSpecialist(Long id);
+    void removeSpecialistFromSubService(Long specialistId, Long subServiceId);
+    void addSpecialistToSubService(Long specialistId, Long subServiceId);
 
 }
--- src/main/java/repository/specialistRepository/SpecialistRepositoryImpl.java
+++ src/main/java/repository/specialistRepository/SpecialistRepositoryImpl.java
@@ -2,14 +2,16 @@ package repository.specialistRepository;
 
 import base.repository.BaseRepositoryImpl;
 import model.Specialist;
+import model.SubService;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-public class SpecialistRepositoryImpl extends BaseRepositoryImpl<Specialist,Long>implements SpecialistRepository {
+public class SpecialistRepositoryImpl extends BaseRepositoryImpl<Specialist, Long> implements SpecialistRepository {
     public SpecialistRepositoryImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
@@ -57,5 +59,134 @@ public class SpecialistRepositoryImpl extends BaseRepositoryImpl<Specialist,Long
         Specialist specialist = query.uniqueResult();
         return Optional.ofNullable(specialist);
 
+    }
+
+    @Override
+    public List<Specialist> getAllSpecialists() {
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            Query<Specialist> query = session.createQuery("FROM Specialist", Specialist.class);
+            List<Specialist> specialists = query.getResultList();
+            session.getTransaction().commit();
+            return specialists;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
+    }
+
+    @Override
+    public Specialist addSpecialist(Specialist specialist) {
+        Session session = sessionFactory.getCurrentSession();
+        try {
+            session.beginTransaction();
+            session.save(specialist);
+            session.getTransaction().commit();
+            return specialist;
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        }
+    }
+
+    @Override
+    public void deleteSpecialist(Long id) {
+        Session session = sessionFactory.getCurrentSession();
+        try {
+            session.beginTransaction();
+            Specialist specialist = session.get(Specialist.class, id);
+            if (specialist != null) {
+                session.delete(specialist);
+            }
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        }
+    }
+
+    @Override
+    public void removeSpecialistFromSubService(Long specialistId, Long subServiceId) {
+        try (Session session = sessionFactory.getCurrentSession()) {
+
+            session.beginTransaction();
+
+            Query query = session.createQuery("update Specialist s set s.subServices = :subServices where s.id = :specialistId");
+
+            query.setParameter("subServices", getSubServicesWithoutSubServiceId(subServiceId, specialistId));
+
+            query.setParameter("specialistId", specialistId);
+
+            query.executeUpdate();
+
+            session.getTransaction().commit();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+    }
+
+    @Override
+    public void addSpecialistToSubService(Long specialistId, Long subServiceId) {
+        try {
+
+            Session session = sessionFactory.getCurrentSession();
+
+
+            Query query = session.createQuery("FROM Specialist s WHERE s.id = :specialistId");
+
+            query.setParameter("specialistId", specialistId);
+
+            Specialist specialist = (Specialist) query.uniqueResult();
+
+
+            query = session.createQuery("FROM SubService s WHERE s.id = :subServiceId");
+
+            query.setParameter("subServiceId", subServiceId);
+
+            SubService subService = (SubService) query.uniqueResult();
+
+
+            if (specialist != null && subService != null) {
+
+                specialist.getSubServices().add(subService);
+
+                subService.getSpecialists().add(specialist);
+
+            }
+
+        } catch (Exception e) {
+
+            String errorMessage = e.getMessage();
+
+            System.err.println("Exception: " + errorMessage);
+        }
+    }
+
+    private List<SubService> getSubServicesWithoutSubServiceId(Long subServiceId, Long specialistId) {
+
+        try (Session session = sessionFactory.getCurrentSession()) {
+
+            session.beginTransaction();
+
+            Query query = session.createQuery("select s.subServices from Specialist s where s.id = :specialistId");
+
+            query.setParameter("specialistId", specialistId);
+
+            List<SubService> subServices = query.list();
+
+            subServices.removeIf(subService -> subService.getId().equals(subServiceId));
+
+            return subServices;
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+            return null;
+
+        }
     }
 }
