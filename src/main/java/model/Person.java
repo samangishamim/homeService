90203324e5aea93c73888c91b89a04fc4d165deb
shamim <shamimsamangi5@gmail.com>
2024-06-03 16:17:30 +0330

    create the class

---
+++ src/main/java/model/Person.java
@@ -0,0 +1,36 @@
+package model;
+
+import base.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.SoftDelete;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@SoftDelete
+@ToString(callSuper = true)
+
+@Entity
+@Table(name = "person")
+public class Person extends BaseEntity<Long> {
+    private String firstName ;
+    private String lastName ;
+
+    @Column(name = "email",unique = true)
+    private String email ;
+
+    @ToString.Exclude
+    private String password ;
+
+    @ToString.Exclude
+    private LocalDateTime registrationDateTime ;
+
+}
