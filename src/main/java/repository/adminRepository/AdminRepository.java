28e874102798a98bd892c812f9e229ce9a66b89d
shamim <shamimsamangi5@gmail.com>
2024-06-03 18:15:51 +0330

    add the others methods for admin

--- src/main/java/repository/adminRepository/AdminRepository.java
+++ src/main/java/repository/adminRepository/AdminRepository.java
@@ -10,22 +10,5 @@ public interface AdminRepository extends BaseRepository<Admin, Long> {
     Optional<Admin> signIn(String username, String password);
 
     List<Admin> findByUsername(String username);
-    List<Service> getAllServices();
-    Optional<Service> addService(Service service);
-    Service updateService(Service service);
-    void deleteService(Long id);
-    List<SubService> getAllSubServices();
-    Optional<SubService> addSubService(SubService subService);
-    SubService updateSubService(SubService subService);
-    void deleteSubService(Long id);
-    List<Customer> getAllCustomers();
-    Customer addCustomer(Customer customer);
-     List<Specialist> getAllSpecialists();
-    Specialist addSpecialist(Specialist specialist);
-    void deleteSpecialist(Long id);
-    void removeSpecialistFromSubService(Long specialistId, Long subServiceId);
-    List<Order> getAllOrders();
-    Order addOrder(Order order);
-    Order updateOrder(Order order);
-    void addSpecialistToSubService(Long specialistId, Long subServiceId);
+
 }
