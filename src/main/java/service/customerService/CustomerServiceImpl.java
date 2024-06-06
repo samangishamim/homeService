872f0a1e88768c44b3629de887a36bb7c5309a38
shamim <shamimsamangi5@gmail.com>
2024-06-07 01:44:07 +0330

    add another method for find the customer

--- src/main/java/service/customerService/CustomerServiceImpl.java
+++ src/main/java/service/customerService/CustomerServiceImpl.java
@@ -3,10 +3,13 @@ package service.customerService;
 import base.service.BaseServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import model.Customer;
+import model.Person;
+import model.Specialist;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import repository.customerRepository.CustomerRepository;
 
+import java.time.Period;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -54,31 +57,7 @@ public class CustomerServiceImpl extends BaseServiceImpl<Customer, Long, Custome
         }
     }
 
-    @Override
-    public Customer updateCustomer(Customer customer) {
-        try (Session session = sessionFactory.getCurrentSession()) {
-            session.beginTransaction();
 
-            // Get the existing customer from the database
-            Customer existingCustomer = session.get(Customer.class, customer.getId());
-
-
-            // Update the properties of the existing customer
-            existingCustomer.setFirstName(customer.getFirstName());
-            existingCustomer.setLastName(customer.getLastName());
-            existingCustomer.setEmail(customer.getEmail());
-            existingCustomer.setPassword(customer.getPassword());
-            existingCustomer.setCredit(customer.getCredit());
-
-
-            // Save the updated customer to the database
-            session.save(existingCustomer);
-            session.getTransaction().commit();
-            return existingCustomer;
-        } catch (Exception e) {
-            return null;
-        }
-    }
 
     @Override
     public void deleteCustomer(Long id) {
@@ -108,5 +87,10 @@ public class CustomerServiceImpl extends BaseServiceImpl<Customer, Long, Custome
         } catch (Exception e) {
             return null;
         }
+    }
+
+    @Override
+    public Customer findByEmailAndPassword(String email, String password) {
+        return repository.findByEmailAndPassword(email, password);
     }
 }
