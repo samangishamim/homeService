c4d8ca594fb3e29bce96453e609387009b3926e2
shamim <shamimsamangi5@gmail.com>
2024-06-07 01:54:22 +0330

    add new imports

--- src/main/java/repository/proposalRepository/ProposalRepositoryImpl.java
+++ src/main/java/repository/proposalRepository/ProposalRepositoryImpl.java
@@ -1,18 +1,22 @@
 package repository.proposalRepository;
 
 import base.repository.BaseRepositoryImpl;
+import model.Order;
 import model.Proposal;
+import model.Specialist;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-public class ProposalRepositoryImpl extends BaseRepositoryImpl<Proposal, Long> implements ProposalRepository{
+public class ProposalRepositoryImpl extends BaseRepositoryImpl<Proposal, Long> implements ProposalRepository {
     public ProposalRepositoryImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
+
     @Override
     public Class<Proposal> getEntityClass() {
         return Proposal.class;
@@ -49,14 +53,12 @@ public class ProposalRepositoryImpl extends BaseRepositoryImpl<Proposal, Long> i
 
 
     @Override
-
     public Optional<Proposal> findByOrderIdAndSpecialistId(Long orderId, Long specialistId) {
         Session session = sessionFactory.getCurrentSession();
         Query<Proposal> query = session.createQuery("FROM Proposal p WHERE p.order.id = :orderId AND p.specialist.id = :specialistId", Proposal.class);
         query.setParameter("orderId", orderId);
         query.setParameter("specialistId", specialistId);
         Proposal proposal = query.uniqueResult();
-
         return Optional.ofNullable(proposal);
 
     }
@@ -84,5 +86,28 @@ public class ProposalRepositoryImpl extends BaseRepositoryImpl<Proposal, Long> i
         Session session = sessionFactory.getCurrentSession();
         session.update(proposal);
         return proposal;
+    }
+
+    @Override
+    public List<Proposal> findByOrder(Order order) {
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            Query<Proposal> query = session.createQuery("FROM Proposal p WHERE p.order = :order", Proposal.class);
+            query.setParameter("order", order);
+            List<Proposal> proposals = query.getResultList();
+            session.getTransaction().commit();
+            return proposals;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
+    }
+
+    @Override
+    public List<Proposal> getProposalsBySpecialist(Boolean isAccepted) {
+        Session session = sessionFactory.getCurrentSession();
+            Query<Proposal> query = session.createQuery("FROM Proposal p WHERE p.isAccepted=:isAccepted", Proposal.class);
+            query.setParameter("isAccepted", isAccepted);
+            return query.getResultList();
     }
 }
