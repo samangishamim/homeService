50bff4e0d05aec4f0b6fb243dfb1c73c68a8d045
shamim <shamimsamangi5@gmail.com>
2024-06-07 01:53:43 +0330

    add new methods needed in menu

--- src/main/java/repository/proposalRepository/ProposalRepository.java
+++ src/main/java/repository/proposalRepository/ProposalRepository.java
@@ -1,7 +1,10 @@
 package repository.proposalRepository;
 
 import base.repository.BaseRepository;
+import model.Order;
 import model.Proposal;
+import model.Specialist;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -15,5 +18,8 @@ public interface ProposalRepository extends BaseRepository<Proposal, Long> {
     List<Proposal> getProposalsByOrderId(Long orderId);
     Proposal addProposal(Proposal proposal);
     Proposal updateProposal(Proposal proposal);
+
+    List<Proposal> findByOrder(Order order);
+    List<Proposal> getProposalsBySpecialist(Boolean isAccepted);
 
 }
