40c97a63bfac98738a0fdde5db89488d6f4f8c21
shamim <shamimsamangi5@gmail.com>
2024-06-07 01:45:35 +0330

    create the main class for testing the menu

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import menu.AdminMenu;
+import menu.MainMenu;
+import model.Customer;
+import service.customerService.CustomerService;
+import utility.ApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MainMenu mainMenu = new MainMenu();
+
+        mainMenu.displayMenu();
+
+
+    }
+
+}
