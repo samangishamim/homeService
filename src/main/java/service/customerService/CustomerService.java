5d71d44168a26cc69d089d5fe3db510fabf1ca60
shamim <shamimsamangi5@gmail.com>
2024-06-07 01:44:00 +0330

    add another method for find the customer

--- src/main/java/service/customerService/CustomerService.java
+++ src/main/java/service/customerService/CustomerService.java
@@ -2,6 +2,7 @@ package service.customerService;
 
 import base.service.BaseService;
 import model.Customer;
+import model.Person;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,7 +12,8 @@ public interface CustomerService  extends BaseService<Customer,Long> {
     Optional<Customer> signIn(String email, String password);
 
     List<Customer> getAllCustomers();
-    Customer updateCustomer(Customer customer);
+
     void deleteCustomer(Long id);
     Customer updateCustomerCredit(Long customerId, double credit);
+    Customer findByEmailAndPassword(String email, String password);
 }
