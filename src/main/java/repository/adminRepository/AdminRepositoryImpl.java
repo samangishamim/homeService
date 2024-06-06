26fdc641d49c3baee831a6a90a1878b6303564a6
shamim <shamimsamangi5@gmail.com>
2024-06-07 01:32:45 +0330

    add one method for finding the username and password by this method

--- src/main/java/repository/adminRepository/AdminRepositoryImpl.java
+++ src/main/java/repository/adminRepository/AdminRepositoryImpl.java
@@ -2,12 +2,10 @@ package repository.adminRepository;
 
 import base.repository.BaseRepositoryImpl;
 import model.*;
-import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,7 +28,6 @@ public class AdminRepositoryImpl extends BaseRepositoryImpl<Admin, Long> impleme
     @Override
     public Optional<Admin> signIn(String username, String password) {
         Session session = sessionFactory.getCurrentSession();
-
         Query<Admin> query = session.createQuery("FROM Admin a WHERE a.username = :username AND a.password = :password", Admin.class);
         query.setParameter("username", username);
         query.setParameter("password", password);
@@ -41,11 +38,20 @@ public class AdminRepositoryImpl extends BaseRepositoryImpl<Admin, Long> impleme
     @Override
     public List<Admin> findByUsername(String username) {
         Session session = sessionFactory.getCurrentSession();
-
         Query<Admin> query = session.createQuery("FROM Admin a WHERE a.username = :username", Admin.class);
-
         query.setParameter("username", username);
-
         return query.getResultList();
+    }
+
+    @Override
+    public Admin findByUserNameAndPassword(String username, String password) {
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        Query<Admin> query = session.createQuery("from Admin where username = :username and password = :password", Admin.class);
+        query.setParameter("username", username);
+        query.setParameter("password", password);
+        Admin admin = query.uniqueResult();
+        session.getTransaction().commit();
+        return admin;
     }
 }
