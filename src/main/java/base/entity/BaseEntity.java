cc1dbda73f3a871918aead6227ea22ce46d0f9a4
shamim <shamimsamangi5@gmail.com>
2024-06-07 01:33:21 +0330

    add superclass annotation

--- src/main/java/base/entity/BaseEntity.java
+++ src/main/java/base/entity/BaseEntity.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 import java.io.Serializable;
 
@@ -15,8 +16,8 @@ import java.io.Serializable;
 @Getter
 @Setter
 @AllArgsConstructor
-
 @MappedSuperclass
+@SuperBuilder
 public class BaseEntity<ID extends Serializable> implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
